ebdd9f3ac66bbc78909e21ba68574d8deb9510c8
julia <juliabadita@live.fr>
2022-03-28 22:11:34 +0200

    trying a counter with react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> This is a test for the counter in react</h1>
+     <Counter  />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+    render() {
+        return (
+            <div>
+             <p> This is my counter</p>
+            </div>
+          );
+    }
+}
+
+export default Counter;
